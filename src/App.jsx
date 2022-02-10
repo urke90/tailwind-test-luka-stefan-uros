032253b85025504ea493b59d53065b24b2746237
urke90 <urosbijelic90@gmail.com>
2022-02-10 14:16:47 +0100

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl text-center font-bold bg-gray-100">
+      <h1 className=""> alal vera </h1>
+    </div>
+  );
+}
+
+export default App;
